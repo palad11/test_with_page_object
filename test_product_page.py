08d7d9aee24f1d00f74af8a6205419b314bb4cdb
palad11 <ivanovm1024@gmail.com>
2019-06-25 01:05:31 +0300

    added new tests

--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,7 @@ import pytest
 from .pages.product_page import ProductPage
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -25,3 +26,24 @@ def test_guest_can_add_product_to_cart(browser, link):
     except Exception as e:
         print(e)
         print(link)
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
