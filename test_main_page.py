7c10ad4975dba7b30e4b418b607eedf82e2850d4
palad11 <ivanovm1024@gmail.com>
2019-06-25 01:45:18 +0300

    added cart checks

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.cart_page import CartPage
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
@@ -23,3 +24,11 @@ def test_guest_can_go_to_login_link(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_cart_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = CartPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    page.should_be_cart_empty()
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 
+from .pages.cart_page import CartPage
 from .pages.product_page import ProductPage
 
 
@@ -27,23 +28,30 @@ def test_guest_can_add_product_to_cart(browser, link):
         print(e)
         print(link)
 
-
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
 
-
+@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = CartPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    page.should_be_cart_empty()
