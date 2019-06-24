9221c6bd18bd6ceb8825cd0cd2d14f1384716ee8
palad11 <ivanovm1024@gmail.com>
2019-06-24 21:10:01 +0300

    fixed timeout

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage(object):
-    def __init__(self, browser,  url, timeout=10):
+    def __init__(self, browser, url, timeout=3):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
