85c5a07d283f09a267f18dd708207c61bea838ad
palad11 <ivanovm1024@gmail.com>
2019-06-24 21:27:39 +0300

    added parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 import pytest
 
-from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
+
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -13,8 +13,8 @@ from .pages.product_page import ProductPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_cart(browser,link):
-    #link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+def test_guest_can_add_product_to_cart(browser, link):
+    # link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
     try:
         page = ProductPage(browser, link)
         page.open()
