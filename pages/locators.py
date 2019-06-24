dcae88b1dc80b0bb71efa6cd929fdecee455b700
palad11 <ivanovm1024@gmail.com>
2019-06-24 21:08:46 +0300

    added locators

---
+++ pages/locators.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class ProductPageLocators(object):
+    CART = (By.CSS_SELECTOR, '#add_to_basket_form > button')
+    ITEM_NAME = (By.XPATH,'//*[@id="content_inner"]/article/div[1]/div[2]/h1')
+    SHOW_ITEM_NAME = (By.CSS_SELECTOR,'#messages > div:nth-child(1) > div > strong')
+    IN_CART_PRICE = (By.CSS_SELECTOR,'div.alertinner > p > strong')
+    ITEM_PRICE = (By.CSS_SELECTOR,'div.col-sm-6.product_main > p.price_color')
