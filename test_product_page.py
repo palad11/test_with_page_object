49ad8fcf2df379211d1f11ca87aaeae47617ce72
palad11 <ivanovm1024@gmail.com>
2019-06-24 21:27:29 +0300

    added parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -3,11 +3,25 @@ import pytest
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 
-def test_guest_can_add_product_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_to_cart()
-    page.solve_quiz_and_get_code()
-    page.should_be_item_added()
-    page.should_be_same_price()
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_cart(browser,link):
+    #link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+    try:
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_cart()
+        page.solve_quiz_and_get_code()
+        page.should_be_item_added()
+        page.should_be_same_price()
+    except Exception as e:
+        print(e)
+        print(link)
